cd8db96537d87ba4e6faa753a7b0ec17b1ddbb79
Paridhi <073bct525.paridhi@pcampus.edu.np>
2025-04-02 13:14:59 -0500

    remove commented lines

--- app.py
+++ app.py
@@ -14,22 +14,7 @@ destination_bucket_name  = os.getenv("DESTINATION_BUCKET_NAME")
 bucket_hash = generate_bucket_hash(source_bucket_name, destination_bucket_name)
 S3DisasterRecoveryStack(app, 
                         f"S3DisasterRecoveryStack-{bucket_hash}",
-                        bucket_hash=bucket_hash 
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
-
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
-
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
-
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
-
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+                        bucket_hash=bucket_hash
     )
 app.synth()
 
