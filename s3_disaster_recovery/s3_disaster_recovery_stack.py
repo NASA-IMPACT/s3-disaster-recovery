1edfd2910f40be450b73e84ef994110ca0895182
Paridhi <073bct525.paridhi@pcampus.edu.np>
2025-04-10 10:49:55 -0500

    ...

--- s3_disaster_recovery/s3_disaster_recovery_stack.py
+++ s3_disaster_recovery/s3_disaster_recovery_stack.py
@@ -57,12 +57,12 @@ class S3DisasterRecoveryStack(Stack):
             destination_bucket_name
         )
 
-        #set_replication = SetUpReplication(self, f"SetUpReplication-{self.bucket_hash}", source_bucket, destination_bucket, source_bucket_name, destination_bucket_name, self.bucket_hash, permissions_boundary_arn )
+        set_replication = SetUpReplication(self, f"SetUpReplication-{self.bucket_hash}", source_bucket, destination_bucket, source_bucket_name, destination_bucket_name, self.bucket_hash, permissions_boundary_arn )
     
         add_lifecyle = AddLifeCycleRule(self, f"AddLifeCycleRule-{self.bucket_hash}", destination_bucket_name, self.bucket_hash, permissions_boundary_arn)
 
-        # if allow_batch_replication:
-        #     start_batch = StartBatchJob(self, f"StartBatchJob-{self.bucket_hash}", source_bucket, destination_bucket, source_bucket_name, destination_bucket_name, self.bucket_hash, permissions_boundary_arn)
+        if allow_batch_replication:
+            start_batch = StartBatchJob(self, f"StartBatchJob-{self.bucket_hash}", source_bucket, destination_bucket, source_bucket_name, destination_bucket_name, self.bucket_hash, permissions_boundary_arn)
 
         # # Output Source Bucket Name
         # CfnOutput(self, f"SourceBucketName-{self.bucket_hash}",
